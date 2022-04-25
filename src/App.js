df061ace07f95639acab4021af95a26922f1cb1d
dendy haidar <dendyhaidar@dendys-MacBook-Air.local>
2022-04-25 12:48:28 +0700

    configuration socketio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return <div className="App"></div>;
+  const [state, setState] = useState({
+    username: "",
+    room: "",
+  });
+
+  const joinARoom = (e) => {
+    e.preventDefault();
+
+    if (state.username !== "" && state.room !== "") {
+      socket.emit("join_room", state.room);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h3>join chat</h3>
+      <input
+        type="text"
+        placeholder="John..."
+        onChange={(e) => setState({ ...state, username: e.target.value })}
+      />
+      <input
+        type="text"
+        placeholder="Room ID..."
+        onChange={(e) => setState({ ...state, room: e.target.value })}
+      />
+
+      <button onClick={joinARoom}>Join a Room</button>
+    </div>
+  );
 }
 
 export default App;
